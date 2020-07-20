2383a1cd4d4438000e95946ad541d7d4d300104c
rumata-ap <avp.ponomarev@gmail.com>
2020-07-20 18:37:16 +0300

    edit

--- Calc/Matrix.cs
+++ Calc/Matrix.cs
@@ -6,6 +6,8 @@ namespace Geo.Calc
    [Serializable]
    public class Matrix
    {
+      private double precalculatedDeterminant = double.NaN;
+
       public double[,] arr { get; private set; }
       int m, n;
       public int N
@@ -53,6 +55,8 @@ namespace Geo.Calc
          }
       }
 
+      public bool IsSquare { get => N == M; }
+
       public Matrix(int N, int M)
       {
          m = M;
@@ -74,6 +78,184 @@ namespace Geo.Calc
          n = source.GetLength(0);
          arr = new double[source.GetLength(0), source.GetLength(1)];
          arr = (double[,])source.Clone();
+      }
+
+      private double Minor(int i, int j)
+      {
+         return ColumnException(j).RowException(i).Determinant();
+      }
+
+      private Matrix RowException(int row)
+      {
+         if (row < 0 || row >= N)
+         {
+            throw new ArgumentException("не верно указан индекс строки");
+         }
+         var result = new Matrix(N - 1, M);
+         result.ProcessFunctionOverData((i, j) => result[i, j] = i < row ? this[i, j] : this[i + 1, j]);
+         return result;
+      }
+
+      private Matrix ColumnException(int column)
+      {
+         if (column < 0 || column >= M)
+         {
+            throw new ArgumentException("не верно указан индекс столбца");
+         }
+         var result = new Matrix(N, M - 1);
+         result.ProcessFunctionOverData((i, j) => result[i, j] = j < column ? this[i, j] : this[i, j + 1]);
+         return result;
+      }
+
+      /// <summary>
+      /// Mетод, позволяющий выполнить какое-либо действие над всеми элементами матрицы.
+      /// </summary>
+      /// <param name="func"></param>
+      public void ProcessFunctionOverData(Action<int, int> func)
+      {
+         for (var i = 0; i < N; i++)
+         {
+            for (var j = 0; j < M; j++)
+            {
+               func(i, j);
+            }
+         }
+      }
+      /// <summary>
+      /// Вычисления определителя матрицы
+      /// </summary>
+      public double Determinant()
+      {
+         if (!double.IsNaN(precalculatedDeterminant))
+         {
+            return precalculatedDeterminant;
+         }
+         if (!IsSquare)
+         {
+            throw new InvalidOperationException("Определитель может быть вычислен только для квадратной матрицы");
+         }
+         if (N == 2)
+         {
+            return this[0, 0] * this[1, 1] - this[0, 1] * this[1, 0];
+         }
+         double result = 0;
+         for (var j = 0; j < M; j++)
+         {
+            result += (j % 2 == 1 ? 1 : -1) * this[1, j] * ColumnException(j).RowException(1).Determinant();
+         }
+         precalculatedDeterminant = result;
+         return result;
+      }
+      /// <summary>
+      /// Вычисление обратной матрицы методом Крамера
+      /// </summary>
+      public Matrix Inverse()
+      {
+         if (M != N) return null;
+
+         double determinant = Determinant();
+         if (Math.Abs(determinant) < 0.0000001) return null;
+
+         Matrix result = new Matrix(N, N);
+         ProcessFunctionOverData((i, j) =>
+         {
+            result[i, j] = ((i + j) % 2 == 1 ? -1 : 1) * Minor(i, j) / determinant;
+         });
+
+         result = result.Transpose();
+         return result;
+      }
+      /// <summary>
+      /// Создание матрицы, образованной последовательным размещением матриц-аргументов друг за другом (слева на право).
+      /// </summary>
+      public static Matrix Augment(params Matrix[] args)
+      {
+         for (int i = 1; i < args.Length; i++)
+         {
+            if (args[i] == null) return null;
+         }
+         int n = args[0].N;
+         int m = args[0].M;
+         for (int i = 1; i < args.Length; i++)
+         {
+            if (args[i].N != n) return null;
+            m += args[i].M;
+         }
+         Matrix res = new Matrix(n, m);
+         int c = 0;
+         for (int k = 0; k < args.Length; k++)
+         {
+            for (int i = 0; i < n; i++)
+            {
+               for (int j = 0; j < args[k].M; j++)
+               {
+                  res[i, c + j] = args[k][i, j];
+               }
+            }
+            c += args[k].M;
+         }
+         return res;
+      }
+
+      /// <summary>
+      /// Создание матрицы, образованной последовательным размещением матриц-аргументов друг под другом.
+      /// </summary>
+      public static Matrix Stack(params Matrix[] args)
+      {
+         for (int i = 1; i < args.Length; i++)
+         {
+            if (args[i] == null) return null;
+         }
+         int n = args[0].N;
+         int m = args[0].M;
+         for (int i = 1; i < args.Length; i++)
+         {
+            if (args[i].M != m || args[i]==null) return null;
+            n += args[i].N;
+         }
+         Matrix res = new Matrix(n, m);
+         int r = 0;
+         for (int k = 0; k < args.Length; k++)
+         {
+            for (int i = 0; i < args[k].N; i++)
+            {
+               for (int j = 0; j < m; j++)
+               {
+                  res[i + r, j] = args[k][i, j];
+               }
+            }
+            r += args[k].N;
+         }
+         return res;
+      }
+
+      public static void PrintConsole(Matrix matrix, int digit)
+      {
+         if (matrix == null) 
+         {
+            Console.WriteLine("Матрица не существует");
+            return;
+         } 
+
+         int max = 0;
+         for (int i = 0; i < matrix.N; i++)
+         {
+            for (int j = 0; j < matrix.M; j++)
+            {
+               string s = Math.Round(matrix[i, j], digit).ToString();
+               if (s.Length > max) max = s.Length;             
+            }
+         }
+         string opt = "{0," + max + "}";
+
+         for (int i = 0; i < matrix.N; i++)
+         {
+            for (int j = 0; j < matrix.M; j++)
+            {
+               Console.Write(String.Format(opt, Math.Round(matrix[i, j],digit)));
+            }             
+            Console.WriteLine();
+         }
       }
 
       public double[,] ToArray()
@@ -107,7 +289,21 @@ namespace Geo.Calc
          return res;
       }
 
-      public static Matrix operator ^(Matrix A, Matrix B)
+      public Matrix Transpose()
+      {
+         Matrix res = new Matrix(M, N);
+         for (int i = 0; i < N; i++)
+         {
+            for (int j = 0; j < M; j++)
+            {
+               res[j, i] = arr[i, j];
+            }
+         }
+
+         return res;
+      }
+
+      public static Matrix operator *(Matrix A, Matrix B)
       {
          if (A.M != B.N) { throw new System.ArgumentException("Не совпадают размерности матриц"); } //Нужно только одно соответствие
          Matrix C = new Matrix(A.N, B.M); //Столько же строк, сколько в А; столько столбцов, сколько в B 
@@ -125,7 +321,7 @@ namespace Geo.Calc
          return C;
       }
 
-      public static Matrix operator *(Matrix A, Matrix B)
+      public static Matrix operator ^(Matrix A, Matrix B)
       {
          if (((A.n != B.n) || (A.m != B.m)) == true) { throw new System.ArgumentException("Не совпадают размерности матриц"); }
          double[,] res = new double[A.n, B.m];
@@ -170,20 +366,6 @@ namespace Geo.Calc
          return new Vector3d(res);
       }
 
-      public static Point3d operator *(Matrix A, Point3d B)
-      {
-         if (A.n != 3 && A.m != 3) { throw new System.ArgumentException("Не верна размерность матрицы"); }
-         double[] res = new double[3];
-
-         for (int i = 0; i < 3; i++)
-         {
-            for (int j = 0; j < 3; j++)
-            {
-               res[i] += A[i, j] * B[j];
-            }
-         }
-         return new Point3d(res);
-      }
 
       public static Matrix operator *(Matrix A, double B)
       {

8488d81c89ede4b3f0bff81d2bb102583ea4fd39
rumata-ap <avp.ponomarev@gmail.com>
2020-08-10 18:29:35 +0300

    edit

---
+++ GMSH/PointG.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geo.GMSH
+{
+   public class PointG : Point3d, ICoordinates
+   {
+      double step;
+      string description;
+
+      public PointG(double x, double y, double z)
+      {
+
+      }
+   }
+}

7c77d85bafdcad5102d0d1297953d20982b4e1d9
rumata-ap <avp.ponomarev@gmail.com>
2020-08-26 16:43:06 +0300

    edit

---
+++ Triangulation/Edge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geo.Triangulation
+{
+   public class Edge
+   {
+      public int A { get; set; }
+      public int B { get; set; }
+
+      public Edge(int a, int b)
+      {
+         A = a;
+         B = b;
+      }
+
+      public bool IsMatch(Edge e)
+      {
+         return (A == e.A && B == e.B) || (A == e.B && B == e.A);
+      }
+   }
+}
